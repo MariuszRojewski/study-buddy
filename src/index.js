e135a3d9d004bef92f94c3551a22bbc08f328d73
MariuszRojewski <115262856+MariuszRojewski@users.noreply.github.com>
2022-10-08 11:21:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from 'views/App';
 
-console.log('dupa');
+console.log('Hello World');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
