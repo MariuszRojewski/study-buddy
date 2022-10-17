2f86bff5a08da1c6681579a1fde8d3930d991b05
unknown <rojewski@webkon.eu>
2022-10-17 13:17:28 +0200

    Ukończona wersja stylowania z buttonem

---
+++ src/components/atoms/Average.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledAverage = styled.div`
+  background-color: blue;
+  color: white;
+  border-radius: 50px;
+  padding: 7px;
+  font-weight: 700;
+`;
---
+++ src/components/atoms/Button.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  width: 25px;
+  height: 25px;
+  border: none;
+  color: white;
+  border-radius: 50px;
+
+  svg {
+    stroke: red;
+    width: 100%;
+    height: 100%;
+  }
+`;
